c4869b303f2dad94a21c292404cb313e3cf539ce
Claire <claire88veiniere@gmail.com>
2015-01-08 20:14:05 +0100

    chgmt speech

--- modules/team.php
+++ modules/team.php
@@ -12,11 +12,13 @@ if (!defined('BASE_URL'))
 <div class="row">
  
 <div class="large-9 push-3 columns panel">
-<h3>Claire</h3>
-<p>Ingénieur Centralienne, je suis passionnée par la puissance des nouvelles technologies, et de
-façon générale par tout ce qui est de l'ordre de la créativité et de l'entreprenariat. Après avoir réalisé pour le compte de l'administration française,
-une <em>application web</em> de gestion du personnel et travaillé dans une agence web, je réalise maintenant des projets informatiques de A jusqu'à Z.
-<p>Ce qui me plaît le plus est de concevoir, m'investir dans un projet et de le rendre, pour l'utilisateur, fiable, rapide et presque indispensable !</p>
+<h3>Claire <small> Responsable projets web</small></h3>
+<p>Je suis passionnée par la puissance et la portée des nouvelles technologies, et de
+façon générale par tout ce qui est de l'ordre de la créativité et de l'entreprenariat. 
+Après avoir réalisé pour le compte de l'ambassade de France aux États-Unis,
+une <em>application web</em> de gestion du personnel sur l'ensemble du continent Amérique du Nord et
+ travaillé dans une agence web, je réalise maintenant des projets informatiques de A jusqu'à Z en collaboration avec Victor. Ma formation et mon diplôme d'ingénieure Centralienne
+ (spécialités Informatique & Finance) me permettent de concevoir, de m'investir dans les différentes facettes d'un projet avec l'objectif de le rendre, pour l'utilisateur, fiable et agréable.</p>
 </div>
  
  
@@ -29,13 +31,11 @@ une <em>application web</em> de gestion du personnel et travaillé dans une agen
 
 <div class="row">
  
- 
 <div class="large-9 push-3 columns panel">
-<h3>Victor</h3>
-<p>Polytechnicien et ayant travaillé dans le domaine scientifique, j'ai développé de très solides compétences dans la création 
-	d'algorithmes rapides et efficaces.</p>
-<p>Dans un projet informatique, mis à part le design et l'expérience utilisateur, il est fondamental que le fonctionnement 
-	repose sur un code propre, rapide, intelligent et facilement modulable.</p>
+<h3>Victor <small> Responsable projets iOS et expert technique</small></h3>
+<p>Polytechnicien et ayant effectué un doctorat à l'École Normale Supérieure (ENS), j'ai acquis une expertise dans le développement d'algorithmes rapides et efficaces. 
+Je m’intéresse en particulier aux questions de performance et de scalabilité. 
+<p>Je garantis que les projets que nous traitons repose sur un code propre, rapide, intelligent et facilement modulable.</p>
 </div>
  
  
